aa03cc087d05a30d0d659fa0a29877d3370657c7
nekrasovmi <nmihail88@gmail.com>
2025-10-29 16:47:57 +0700

    555

--- sorter/IntegratingForm.cs
+++ sorter/IntegratingForm.cs
@@ -17,7 +17,38 @@ namespace sorter
         {
             InitializeComponent();
             Icon = Properties.Resources.integralico;
-            DrawFunction();
+            //DrawFunction();
+            
+            double scale = 40;
+            this.DoubleBuffered = true;
+            this.Paint += new PaintEventHandler(OnPaint);
+        }
+
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            DrawAxes(g);
+            //DrawFunction(g, x => Math.Sin(x));
+        }
+
+        private void DrawAxes(Graphics g)
+        {
+            int Width = pictureBox2.Width;
+            int Higth = pictureBox2.Height;
+            double scale = 40;
+            Pen axisPen = new Pen(Color.Black, 2);
+            g.DrawLine(axisPen, 0, Higth/2, Width, Higth/2);
+            g.DrawLine(axisPen, Width/2, 0, Width /2, Higth);
+            Pen gridPen = new Pen(Color.LightGray,1);
+            Font font = new Font("Arial",8);
+            for (int i=-10;i<=10;i++)
+            {
+                int x = Width / 2 + (int)(i * scale);
+                int y = Higth / 2 + (int)(i * scale);
+                g.DrawLine(gridPen, x, 0, x, Higth);
+                g.DrawLine(gridPen, 0, y, Width, y);
+            }
         }
 
         private void DrawFunction()
